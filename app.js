b3bf323944a1d060b64c15830ad02acb4478b304
Muhammad yusuf <muhammadqurbonov@gmail.com>
2026-09-22 08:04:58 +0300

    misollar yechish

---
+++ app.js
@@ -0,0 +1,84 @@
+// function sum(x, y) {
+//   return x + y;
+// }
+// console.log(sum(2, 3));
+// function toSekunds(minutes) {
+//   return minutes * 60;
+// }
+// console.log(toSekunds(2));
+// function C(asos, balandlik) {
+//   return (asos * balandlik) / 2;
+// }
+// console.log(C(7, 4));
+// function kun(age) {
+//   return age * 365;
+// }
+// console.log(kun(2));
+// function pi(son) {
+//   return Math.pow(son, 3);
+// }
+// console.log(pi(3));
+// function firstElement(arr) {
+//   return arr[0];
+// }
+// console.log(firstElement([2, 4, 6]));
+// function power(x, y) {
+//   return x * y;
+// }
+// console.log(power(20, 10));
+// function hourToSekunds(hour) {
+//   return hour * 3200;
+// }
+// console.log(hourToSekunds(2));
+// function uchinchiTomon(tomon1, tomon2) {
+//   returne(tomon1 + tomon2) - 1;
+// }
+// console.log(uchinchiTomon(8, 10));
+// function qoldiq(son1, son2) {
+//   return son1 % son2;
+// }
+// console.log(qoldiq(3, 4));
+// function turtburchakYuzi(boyi, eni) {
+//   return boyi * eni;
+// }
+// console.log(turtburchakYuzi(4, 5));
+// function ti(san) {
+//   return Math.pow(san, 2);
+// }
+// console.log(ti(5));
+// function hi(raqam) {
+//   return raqam >= 0;
+// }
+// console.log(hi(0));
+// function da(n) {
+//   return (n - 2) * 180;
+// }
+// console.log(da(3));
+// function ochkolar(x, y) {
+//   return x * 2 + y * 3;
+// }
+// console.log(ochkolar(1, 1));
+// function re(bi) {
+//   return bi + "edabit";
+// }
+// console.log(re("salom"));
+// function ikkitaSon(x, y) {
+//   return x >= 0 && y >= 0;
+// }
+// console.log(ikkitaSon(40, 38));
+// function oyoqlar(tovuq, qoy, sigir) {
+//   return tovuq * 2 + qoy * 4 + sigir * 4;
+// }
+// console.log(oyoqlar(2, 3, 5));
+// function tengmi(x, y) {
+//   return x == y;
+// }
+// console.log(tengmi(2, 2));
+// function futbolOchko(yutishlar, duranglar, maglubiyatlar) {
+//   return yutishlar + 3 + duranglar + 1 + maglubiyatlar + 0;
+// }
+// console.log(futbolOchko(3, 4, 2));
+function sekundlar(soat, minut) {
+  return soat * 3200 + minut * 60;
+}
+console.log(sekundlar(1, 3));
